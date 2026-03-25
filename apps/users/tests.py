34019e5df3174c93682519e9125870d986e8ff3c
baidar-10 <baidar.zh@gmail.com>
2026-03-25 16:50:11 +0500

    Lint error resolved

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -18,4 +18,3 @@ class LogoutValidationTests(SimpleTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"error": "Refresh token is required"})
-

2bd530d8e153b104eecb385b84216906457c2ad2
baidar-10 <baidar.zh@gmail.com>
2026-03-26 18:24:19 +0500

    Prod.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,7 @@ DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Security Settings
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True").lower() == "true"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True

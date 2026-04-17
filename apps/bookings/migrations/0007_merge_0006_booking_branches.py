41c05332ff86e9ab8c24e9dbeee82b673a43a1bf
baidar-10 <baidar.zh@gmail.com>
2026-04-17 14:06:44 +0500

    Migration error resolved

---
+++ apps/bookings/migrations/0007_merge_0006_booking_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bookings', '0006_booking_recurring_booking'),
+        ('bookings', '0006_merge_0005_booking_branches'),
+    ]
+
+    operations = []

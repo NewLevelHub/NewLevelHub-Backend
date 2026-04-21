5aafce7b4dbea800ec7b00a43f7c2637ab9665b1
Bekzat Yessimkhanuly <esimhanuli13@gmail.com>
2026-04-21 10:49:56 +0500

    Update board count to exclude archived boards in CompanyViewSet

--- apps/companies/views.py
+++ apps/companies/views.py
@@ -378,7 +378,7 @@ class CompanyViewSet(viewsets.ModelViewSet):
                 'max': company.max_employees,
             },
             'boards': {
-                'current': company.boards.count(),
+                'current': company.boards.filter(is_archived=False).count(),
                 'max': company.max_boards,
             },
             'storage': {

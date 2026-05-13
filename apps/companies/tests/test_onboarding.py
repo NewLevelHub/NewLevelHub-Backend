715038a4ab593be7a7a848c9368b5b0a71df4a39
baidar-10 <baidar.zh@gmail.com>
2026-05-13 14:52:24 +0500

    Test fail resolved

--- apps/companies/tests/test_onboarding.py
+++ apps/companies/tests/test_onboarding.py
@@ -309,7 +309,7 @@ class TestInvitedCompanyAdminBoardOnboarding:
         )
         assert reg.status_code == status.HTTP_201_CREATED
         new_user = User.objects.get(email='invited.admin@onboarding-board.test')
-        assert new_user.is_email_verified is True
+        assert new_user.is_email_verified is False
 
         auth(api_client, new_user)
         board_resp = api_client.post(BOARDS_URL, {'name': 'Onboarding Board'}, format='json')
